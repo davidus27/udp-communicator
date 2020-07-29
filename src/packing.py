3cce391fe403fdac254cf5b5f838d5d3c72a798c
davidus27 <davidisbest27@gmail.com>
2020-07-29 16:19:00 +0200

    renamed functions and removed constants from the packing module

--- src/packing.py
+++ src/packing.py
@@ -1,19 +1,6 @@
-import struct
-
-CODING_FORMAT = "utf-8"
-SEGMENTS_AMOUNT = 10 # how many segments will be sent at once
-ACK = b"OK"
-NACK = b"ERROR"
-KEEP_ALIVE = b"KA"
-ENDING = b"END."
+import constants
 
 class FragmentPacking():
-    # TODO: write down all these packing formats
-    STARTING_HEADER = "bIhh" # Data Type + Fragments amount + Fragment size + Checksum
-    TEXT_MESSAGE_HEADER = "xh" # Checksum
-    DATA_HEADER = "ih"
-
-
     def __init__(self, data, data_type: str, fragment_size: int, file_name=None):
         self.data = data
         self.data_size = len(data)
@@ -21,29 +8,18 @@ class FragmentPacking():
         self.file_name = file_name
         self.fragment_size = fragment_size
 
-    def _get_chunk(self, sub_array, segment_size, offset):
+    def _split_package_data(self, sub_array, segment_size, offset):
         """ Splits package to the even sized segments"""
-        return [sub_array[i: i+segment_size].encode(CODING_FORMAT) for i in range(0,len(sub_array), segment_size)]
-
+        return [sub_array[i: i+segment_size] for i in range(0,len(sub_array), segment_size)]
 
     def split_data(self):
         """
         Splits the data into packages
         """
-        segment_size = self.fragment_size - 6
-        package_size = segment_size * SEGMENTS_AMOUNT
-
-        #yield [struct.pack(self.TEXT_MESSAGE_HEADER, self.calculate_checksum())]
-        
+        package_size = self.fragment_size * constants.SEGMENTS_AMOUNT
         for offset in range(0, self.data_size, package_size):
-            sub_array = self.data[offset: offset + package_size]
-            #yield self._get_chunk(sub_array, segment_size, offset) 
-            yield self._get_chunk(sub_array, segment_size, offset)
-
-
-
-    def calculate_checksum(self):
-        return 10
+            package_data = self.data[offset: offset + package_size]
+            yield self._split_package_data(package_data, self.fragment_size, offset)
 
     def calculate_fragments_amount(self):
         return len(self.data)/self.fragment_size
