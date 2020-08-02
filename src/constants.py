8f929c3a0c6d3a76f061c97a9e9a0aa98b0c1634
davidus27 <davidisbest27@gmail.com>
2020-08-03 01:33:50 +0200

    adding constants and struct calcsize

--- src/constants.py
+++ src/constants.py
@@ -1,11 +1,17 @@
+import struct
+
 CODING_FORMAT = "utf-8"
 ACK = b"OK"
 NACK = b"ERROR"
 KEEP_ALIVE = b"KA"
 ENDING = b"END."
-STARTING_HEADER = "ihhc" # Fragments amount + Fragment size + Checksum + Data Type
-DATA_HEADER = "ih"
+STARTING_HEADER = "ihc" # Fragments amount + Fragment size + Data Type
+DATA_HEADER = "iB"
 SEGMENTS_AMOUNT = 10 # how many segments will be sent at once
+STARTING_HEADER_SIZE = struct.calcsize(STARTING_HEADER) # bytes
+DATA_HEADER_SIZE = struct.calcsize(DATA_HEADER)
+MAX_FILE_NAME_SIZE = 255
 MAXIMUM_THREADS = 5
-STARTING_HEADER_SIZE = 9 # bytes
-DATA_HEADER_SIZE = 6 # bytes
+
+MIN_FRAGMENT_SIZE = 50
+MAX_FRAGMENT_SIZE = 1466
